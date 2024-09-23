019d28952b2e4ac6f851a7418b457899233054fd
K0T1KK <rich050606@gmail.com>
2024-09-23 10:50:04 +0900

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,29 @@
+fun main() {
+    // Целочисленные значения
+    val age: Int = 18
+    val pop: Long = 140_000_000L
+    val temp: Short = +9
+    val maxByte: Byte = 127
+
+    // Числа с плавающей точкой
+    val p: Double = 3.14159
+    val e: Float = 2.718f
+
+    // Символ и строка
+    val bukva: Char = 'K'
+    val hello: String = "Hello, c++"
+
+    // Логическое значение
+    val c: Boolean = true
+
+    // Вывод значений
+    println("Возраст: $age")
+    println("Население России: $pop")
+    println("Температура: $temp")
+    println("Максимальное значение байта: $maxByte")
+    println("Число Пи: $p")
+    println("Число e: $e")
+    println("Буква: $bukva")
+    println("Приветствие: $hello")
+    println("с++ - это здорово? $c")
+}
